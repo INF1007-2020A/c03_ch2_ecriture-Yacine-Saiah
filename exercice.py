d25bf2fb3df3cc22f2cba1382fe56e98b13e1c89
Yacine Djilali-Saiah <yacinedjilali-saiah@Yacines-MacBook-Pro.local>
2020-10-17 22:29:36 -0400

    terminé

--- exercice.py
+++ exercice.py
@@ -3,10 +3,8 @@
 def majuscule(mot):
     resultat = ''
     for lettre in mot:
-        # TODO completer la fonction ici
-        
-        resultat += lettre
-    return mot
+       resultat += chr(ord(lettre) - 32) 
+    return resultat
 
 
 if __name__ == '__main__':
